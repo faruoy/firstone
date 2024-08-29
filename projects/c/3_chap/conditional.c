a528ceb631c6367f91a0b46240f275b3625393a6
faruoy <farhanmohammed8099@gmail.com>
2024-08-29 15:48:54 +0530

    hao

--- projects/c/3_chap/conditional.c
+++ projects/c/3_chap/conditional.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main(){
        int age;
+       printf("Enter your age\n");
        scanf("%d", &age);
-
-       if(age>18) {
+       
+       if(age>=18) {
           printf("Yes, You are Eligible to Drive\n\n");
-                  }
-       else{
-        printf("Sorry You aren't Eligible to Drive\n\n");
-       }
+                }
+       else if(age>=16) {
+       printf("You can now drive vehicles under 50cc");
+       }  
+      else{
+      printf("Sorry You cannot drive :)");}
  return 0;                 
 }
