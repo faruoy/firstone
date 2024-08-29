3c982e14f9d2c1c9140fd68cfb0255146cb59d73
faruoy <farhanmohammed8099@gmail.com>
2024-08-29 19:59:12 +0530

    yo

--- projects/c/3_chap/ternary.c
+++ projects/c/3_chap/ternary.c
@@ -4,5 +4,6 @@ int a=567 , b=3200;
 // condition ? expression-if-true : expression-if-false 
 // Here "?" and ":" are called Ternary Operators 
 a>b?printf("a is greater than b\n"):printf("b is greater than a\n");
+a<b?printf("yo\n"):printf("ho");
 return 0;
 }
