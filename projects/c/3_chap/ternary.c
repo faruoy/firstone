c317ebe05e2e9f58f3addf3665457bfceef7af20
faruoy <farhanmohammed8099@gmail.com>
2024-08-29 14:46:00 +0000

    abslute

--- projects/c/3_chap/ternary.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-int a=567 , b=3200;
-// condition ? expression-if-true : expression-if-false 
-// Here "?" and ":" are called Ternary Operators 
-a>b?printf("a is greater than b\n"):printf("b is greater than a\n");
-a<b?printf("yo\n"):printf("ho");
-return 0;
-}
