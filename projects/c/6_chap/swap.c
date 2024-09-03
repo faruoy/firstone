84a65633550ddfd0ded38aaa17934efe89c2d9e4
faruoy <farhanmohammed8099@gmail.com>
2024-09-03 06:20:57 +0530

    l

--- projects/c/6_chap/swap.c
+++ projects/c/6_chap/swap.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
-void swap(int,int);
+void swap(int*,int*);
 void swap(int* a,int *b){
     int temp;
-    *a=temp;
-    *b=*a;
-    temp=*b;
-
-
+    temp=*a;
+    *a=*b;
+    *b=temp;
 }
+int main(){
+	int a=65;
+	int b=2;
+	swap(&a,&b);
+printf("value of a is %d\n",a);
+printf("value of b is %d\n",b);
+
+return 0;
+}
+	
