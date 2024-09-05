3b4b1a93f4c88d699627a07fa00c8785a525f8cc
faruoy <farhanmohammed8099@gmail.com>
2024-09-05 09:40:08 +0000

    hello

--- projects/c/5_chap/practice/04.c
+++ projects/c/5_chap/practice/04.c
@@ -2,16 +2,16 @@
 // Write a program using recursion to calculate nth element of Fibonacci series.
 // 0,1,1,2,3,5,8,13,21,34...........nth
 // fibo(n)=fibo(n-1)+fibo(n-2)
-int fibo(int);
-int fibo(int n){
+int series(int);
+int series(int n){
     if(n==1 || n==2){ // base case
         return n-1;
     }
-    return fibo(n-1)+fibo(n-2);
+    return series(n-1)+series(n-2);
      
     }
 
     int main(){
         int n=6;
-        printf("The value %dth value of Fibonacci series is %d\n",n,fibo(n));
+        printf("The value %dth value of Fibonacci series is %d\n",n,series(n));
     }
