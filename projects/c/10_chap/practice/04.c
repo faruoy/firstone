c2dbf2496c38d86ecbd87892054aff23bda4e296
faruoy <farhanfarhann91@gmail.com>
2024-09-24 09:33:36 +0530

    yes

---
+++ projects/c/10_chap/practice/04.c
@@ -0,0 +1,30 @@
+/*Take name and salary of two employees as input from the user and write them to 
+a text file in the following format:
+i. Name1, 3300
+ii. Name2, 7700*/
+typedef struct employee{
+char name[];
+int salary;
+}emp;
+#include <stdio.h>
+int main() {
+emp e1,e2;
+ptr=fopen("employee.txt","w");
+printf("Enter the Name of Employee 1 : ");
+scanf("%s",&e1.name);
+e1.salary=3300;
+printf("Enter the Name of Emplo    yee 2 : ")
+scanf("%s",&e2.name);
+e2.salary=7700;
+FILE *ptr;
+ptr=fopen("employ.txt","w");
+fprintf(ptr,"%s",e1.name);
+fprintf(ptr,"%d",e1.salary);
+fprintf(ptr,"%s",e2.name);
+fprintf(ptr,"%d",e2.salary);
+fclose(ptr);
+
+	return 0;
+
+
+}
