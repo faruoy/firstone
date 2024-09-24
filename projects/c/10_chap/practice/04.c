09b2efeb35ad0dbd2af8016e9c1c08989dd793df
faruoy <farhanfarhann91@gmail.com>
2024-09-24 09:57:59 +0530

    yes

--- projects/c/10_chap/practice/04.c
+++ projects/c/10_chap/practice/04.c
@@ -3,25 +3,28 @@ a text file in the following format:
 i. Name1, 3300
 ii. Name2, 7700*/
 typedef struct employee{
-char name[];
+char name[20];
 int salary;
 }emp;
 #include <stdio.h>
 int main() {
 emp e1,e2;
-ptr=fopen("employee.txt","w");
 printf("Enter the Name of Employee 1 : ");
-scanf("%s",&e1.name);
+scanf("%s",e1.name);
 e1.salary=3300;
 printf("Enter the Name of Employee 2 : ");
-scanf("%s",&e2.name);
+scanf("%s",e2.name);
 e2.salary=7700;
 FILE *ptr;
 ptr=fopen("employ.txt","w");
 fprintf(ptr,"%s",e1.name);
+fprintf(ptr,"%c","\n");
 fprintf(ptr,"%d",e1.salary);
+fprintf(ptr,"%c","\n");
 fprintf(ptr,"%s",e2.name);
+fprintf(ptr,"%c","\n");
 fprintf(ptr,"%d",e2.salary);
+fprintf(ptr,"%c","\n");
 fclose(ptr);
 
 	return 0;
